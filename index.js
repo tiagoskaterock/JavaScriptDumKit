6bc448373bfe5e34d7e5153a6e2bf81db71038b6
tiago <tiagolemespalhano@gmail.com>
2021-12-21 12:22:59 -0300

    background images

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ let numberOfDrumButtons = document.querySelectorAll('.drum').length
 for (let i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll('.drum')[i].addEventListener('click', function handleClick() {
-        alert("I cliked ");
+        
+        let audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+
     });
     
 }
